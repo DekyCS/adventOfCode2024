6c8f2f25f660d8d822ae3c4a6473ad3ab528ab26
omarl <omarlahmimi@gmail.com>
2024-12-06 23:37:55 -0500

    complete day 5 part 2

--- day5.py
+++ day5.py
@@ -6,7 +6,9 @@ orderRule = []
 updates = []
 
 goodUpdates = []
+badUpdates = []
 middleCount = 0
+middleCountFixed = 0
 
 for line in f:
     if "|" in line:
@@ -29,23 +31,41 @@ def find_all_num_before(num):
 def find_middle(arr):
     return arr[math.floor(len(arr)/2)]
 
-
-for update in updates:
-    updateGood = True
+def check_order(update):
     for i in range(len(update)):
         if i == len(update) - 1:
             continue
         num_before = find_all_num_before(update[i])
-        for j in range(len(update) - i):
-            if update[j + i] in num_before:
-                updateGood = False
+        for j in range(len(update) - i - 1):
+            if update[j + i + 1] in num_before:
+                return i, j + i + 1
+    return -1,-1 #usually -1 means it doesn't but in this code it does
+
+def swap (a,b, arr):
+    temp = arr[a]
+    arr[a] = arr[b]
+    arr[b] = temp
+
+#Find the bad updates
+for update in updates:
+    result = check_order(update)
+    if result != (-1,-1):
+        badUpdates.append(update)
+        swap(result[0], result[1], update)
+        while True:
+            result = check_order(update)
+            if result == (-1,-1):
                 break
-        if not updateGood:
-            break
-    if updateGood:
+            else:
+                swap(result[0], result[1], update)
+
+    elif result == (-1,-1):
         goodUpdates.append(update)
 
 for goodUpdate in goodUpdates:
     middleCount += find_middle(goodUpdate)
 
-print(middleCount)
+for badUpdate in badUpdates:
+    middleCountFixed += find_middle(badUpdate)
+
+print(middleCountFixed)
